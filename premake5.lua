bdca12592f03107558c1178c675b35f4d42a3e11
Gabriel Mañeru <gabrielmaneru@gmail.com>
2022-08-15 12:19:43 +0100

    Project Cleanup

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "spdlog"
 	kind "StaticLib"
 	language "C++"
+	staticruntime "On"
 
 	targetdir ("build/" .. outputdir .. "/%{prj.name}/bin")
 	objdir ("build/" .. outputdir .. "/%{prj.name}/obj")
@@ -18,18 +19,9 @@ project "spdlog"
 	
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
 
 		defines 
 		{ 
 			"SPDLOG_COMPILED_LIB",
 			"_CRT_SECURE_NO_WARNINGS"
 		}
-
-	filter "configurations:debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:release"
-		runtime "Release"
-		optimize "on"
